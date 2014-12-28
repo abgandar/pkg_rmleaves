79ee67b6a584764115847fcbf5d38dc50a42ca75
alex <alex@cbcf0ede-832c-0410-b4e8-88e0f7be8512>
2014-12-28 19:41:53 +0000

    update

--- pkg_rmleaves/package.sh
+++ pkg_rmleaves/package.sh
@@ -4,18 +4,23 @@
 # Create the package for this port
 DATE=`date +%Y%m%d`
 DIR="pkg_rmleaves-$DATE"
+TAR="$DIR.tar.gz"
 
 # Create the tar ball
 mkdir "$DIR"
 cp pkg_rmleaves pkg_rmleaves.1 "$DIR"
-tar -cf "$DIR.tar.gz" "$DIR"
+tar -cf "$TAR" "$DIR"
 rm -rf "$DIR"
 
 # Update the port file
 sed -i "" "s/^PORTVERSION=.*/PORTVERSION=	$DATE/" port/Makefile
-cp "$DIR.tar.gz" /usr/ports/distfiles
+cp "$TAR" /usr/ports/distfiles
 cd port
 make makesum
+portlint
+rm "/usr/ports/distfiles/$TAR"
 cd ..
 
+# Create port diff ready for sending the PR
+diff -u /usr/ports/ports-mgmt/pkg_rmleaves port > port.diff
 
