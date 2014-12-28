4bd5ebd27349962e3f30dc163231f895366ae956
alex <alex@cbcf0ede-832c-0410-b4e8-88e0f7be8512>
2014-12-28 19:33:01 +0000

    Also automatically update the port file

--- pkg_rmleaves/package.sh
+++ pkg_rmleaves/package.sh
@@ -11,3 +11,11 @@ cp pkg_rmleaves pkg_rmleaves.1 "$DIR"
 tar -cf "$DIR.tar.gz" "$DIR"
 rm -rf "$DIR"
 
+# Update the port file
+sed -i "" "s/^PORTVERSION=.*/PORTVERSION=	$DATE/" port/Makefile
+cp "$DIR.tar.gz" /usr/ports/distfiles
+cd port
+make makesum
+cd ..
+
+
